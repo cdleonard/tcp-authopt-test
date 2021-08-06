e04949bfac84763b3b659bcdaf0a3c7ebe3e6ec8
Leonard Crestez <cdleonard@gmail.com>
2021-08-06 23:05:06 +0300

    sockaddr: ipv6 support
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/sockaddr.py
+++ tcp_authopt_test/sockaddr.py
@@ -2,7 +2,7 @@
 import socket
 import struct
 from dataclasses import dataclass
-from ipaddress import IPv4Address
+from ipaddress import IPv4Address, IPv6Address
 
 
 @dataclass
@@ -32,6 +32,47 @@ class sockaddr_in:
 
 
 @dataclass
+class sockaddr_in6:
+    """Like sockaddr_in6 but for python. Always contains scope_id"""
+
+    port: int
+    addr: IPv6Address
+    flowinfo: int
+    scope_id: int
+    sizeof = 28
+
+    def __init__(self, port=0, addr=None, flowinfo=0, scope_id=0):
+        self.port = port
+        if addr is None:
+            addr = IPv6Address(0)
+        self.addr = IPv6Address(addr)
+        self.flowinfo = flowinfo
+        self.scope_id = scope_id
+
+    def pack(self):
+        return struct.pack(
+            "HHI16sI",
+            socket.AF_INET6,
+            self.port,
+            self.flowinfo,
+            self.addr.packed,
+            self.scope_id,
+        )
+
+    @classmethod
+    def unpack(cls, buffer):
+        family, port, flowinfo, addr_packed, scope_id = struct.unpack(
+            "HHI16sI", buffer[:28]
+        )
+        if family != socket.AF_INET6:
+            raise ValueError(f"Must be AF_INET6 not {family}")
+        return cls(port, addr_packed, flowinfo=flowinfo, scope_id=scope_id)
+
+    def __bytes__(self):
+        return self.pack()
+
+
+@dataclass
 class sockaddr_storage:
     family: int
     data: bytes
@@ -53,5 +94,7 @@ def sockaddr_unpack(buffer: bytes):
     family = struct.unpack("H", buffer[:2])[0]
     if family == socket.AF_INET:
         return sockaddr_in.unpack(buffer)
+    elif family == socket.AF_INET6:
+        return sockaddr_in6.unpack(buffer)
     else:
         return sockaddr_storage.unpack(buffer)
