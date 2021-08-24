71911aaba34d8089b2e58e06d0aa483843b25ef5
Leonard Crestez <cdleonard@gmail.com>
2021-08-24 20:26:42 +0300

    run.sh: Update
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- run.sh
+++ run.sh
@@ -1,3 +1,11 @@
-#! /bin/sh
+#! /bin/bash
 
-exec tox -- --log-cli-level=DEBUG -s "$@"
+if ! command -v tox >/dev/null; then
+	echo >&2 "error: please install the python tox package"
+	exit 1
+fi
+if [[ $(id -u) -ne 0 ]]; then
+	echo >&2 "warning: running as non-root user is unlikely to work"
+fi
+cd "$(dirname "${BASH_SOURCE[0]}")"
+exec tox "$@"
