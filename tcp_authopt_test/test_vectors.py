005011b879a76479b65ec9dc4d60eed7e8bc2067
Leonard Crestez <cdleonard@gmail.com>
2021-08-24 19:51:05 +0300

    test_vectors: Cleanup, much of this is no longer useful
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_vectors.py
+++ tcp_authopt_test/test_vectors.py
@@ -67,81 +67,38 @@ class TestIETFVectors:
         assert mac.hex(" ") == mac_hex
 
     def test_4_1_1(self):
-        packet_hex = """
+        self.check(
+            """
             45 e0 00 4c dd 0f 40 00 ff 06 bf 6b 0a 0b 0c 0d
             ac 1b 1c 1d e9 d7 00 b3 fb fb ab 5a 00 00 00 00
             e0 02 ff ff ca c4 00 00 02 04 05 b4 01 03 03 08
             04 02 08 0a 00 15 5a b7 00 00 00 00 1d 10 3d 54
             2e e4 37 c6 f8 ed e6 d7 c4 d6 02 e7
-        """
-        traffic_key = bytes.fromhex(
-            "6d 63 ef 1b 02 fe 15 09 d4 b1 40 27 07 fd 7b 04 16 ab b7 4f"
+            """,
+            "6d 63 ef 1b 02 fe 15 09 d4 b1 40 27 07 fd 7b 04 16 ab b7 4f",
+            "2e e4 37 c6 f8 ed e6 d7 c4 d6 02 e7",
+            self.client_isn_41x,
+            0,
         )
-        mac = bytes.fromhex("2e e4 37 c6 f8 ed e6 d7 c4 d6 02 e7")
-
-        p = IP(bytes.fromhex(packet_hex))
-        assert p[IP].src == str(self.client_ipv4)
-        assert p[IP].dst == str(self.server_ipv4)
-        assert p[TCP].flags.S == True
-        assert p[TCP].flags.A == False
-        assert p[TCP].dport == 179
-        assert p[TCP].sport == 0xE9D7
-        assert p[TCP].ack == 0
-        assert p[TCP].chksum == 0xCAC4
-        assert p[TCP].dataofs == 14
-        assert p[TCP].seq == self.client_isn_41x
-        assert p[TCP].ack == 0
-
-        context_bytes = build_context_from_scapy_syn(p)
-        logger.info("context: %s", context_bytes.hex(" "))
-        assert kdf_sha1(self.master_key, context_bytes).hex() == traffic_key.hex()
-
-        packet_bytes = bytes.fromhex(packet_hex)
-        message_bytes = bytes.fromhex("00 00 00 00")
-        message_bytes += self.client_ipv4.packed
-        message_bytes += self.server_ipv4.packed
-        message_bytes += bytes.fromhex("00 06 00 38")
-        message_bytes += packet_bytes[0x14:0x24]
-        message_bytes += bytes.fromhex("00 00")
-        message_bytes += packet_bytes[0x26:0x40]
-        message_bytes += b"\x00" * 12
-
-        assert (
-            message_bytes.hex()
-            == build_message_from_scapy(p, include_options=True).hex()
-        )
-
-        logger.info("message: %s", message_bytes.hex(" "))
-        assert mac_sha1(traffic_key, message_bytes).hex() == mac.hex()
 
     def test_4_1_2(self):
-        packet_hex = """
+        self.check(
+            """
             45 e0 00 4c 65 06 40 00 ff 06 37 75 ac 1b 1c 1d
             0a 0b 0c 0d 00 b3 e9 d7 11 c1 42 61 fb fb ab 5b
             e0 12 ff ff 37 76 00 00 02 04 05 b4 01 03 03 08
             04 02 08 0a 84 a5 0b eb 00 15 5a b7 1d 10 54 3d
             ee ab 0f e2 4c 30 10 81 51 16 b3 be
-        """
-        traffic_key = bytes.fromhex(
-            """d9 e2 17 e4 83 4a 80 ca 2f 3f d8 de 2e 41 b8 e6 79 7f ea 96"""
+            """,
+            "d9 e2 17 e4 83 4a 80 ca 2f 3f d8 de 2e 41 b8 e6 79 7f ea 96",
+            "ee ab 0f e2 4c 30 10 81 51 16 b3 be",
+            self.server_isn_41x,
+            self.client_isn_41x,
         )
-        mac = bytes.fromhex("ee ab 0f e2 4c 30 10 81 51 16 b3 be")
-
-        p = IP(bytes.fromhex(packet_hex))
-        assert p[IP].src == str(self.server_ipv4)
-        assert p[IP].sport == 179
-        assert p[TCP].flags.S == True
-        assert p[TCP].flags.A == True
-        assert p[TCP].seq == self.server_isn_41x
-        assert p[TCP].ack == self.client_isn_41x + 1
-        context_bytes = build_context_from_scapy_synack(p)
-        logger.info("context: %s", context_bytes.hex(" "))
-        assert kdf_sha1(self.master_key, context_bytes).hex(" ") == traffic_key.hex(" ")
-        message_bytes = build_message_from_scapy(p, include_options=True)
-        assert mac_sha1(traffic_key, message_bytes).hex(" ") == mac.hex(" ")
 
     def test_4_1_3(self):
-        packet_hex = """
+        self.check(
+            """
             45 e0 00 87 36 a1 40 00 ff 06 65 9f 0a 0b 0c 0d
             ac 1b 1c 1d e9 d7 00 b3 fb fb ab 5b 11 c1 42 62
             c0 18 01 04 a1 62 00 00 01 01 08 0a 00 15 5a c1
@@ -151,23 +108,16 @@ class TestIETFVectors:
             26 02 06 01 04 00 01 00 01 02 02 80 00 02 02 02
             00 02 02 42 00 02 06 41 04 00 00 da bf 02 08 40
             06 00 64 00 01 01 00
-        """
-        traffic_key = bytes.fromhex(
-            """d2 e5 9c 65 ff c7 b1 a3 93 47 65 64 63 b7 0e dc 24 a1 3d 71"""
+            """,
+            "d2 e5 9c 65 ff c7 b1 a3 93 47 65 64 63 b7 0e dc 24 a1 3d 71",
+            "70 64 cf 99 8c c6 c3 15 c2 c2 e2 bf",
+            self.client_isn_41x,
+            self.server_isn_41x,
         )
-        mac = bytes.fromhex("70 64 cf 99 8c c6 c3 15 c2 c2 e2 bf")
-
-        p = IP(bytes.fromhex(packet_hex))
-        context_bytes = build_context_from_scapy(
-            p, self.client_isn_41x, self.server_isn_41x
-        )
-        logger.info("context: %s", context_bytes.hex(" "))
-        assert kdf_sha1(self.master_key, context_bytes).hex(" ") == traffic_key.hex(" ")
-        message_bytes = build_message_from_scapy(p, include_options=True)
-        assert mac_sha1(traffic_key, message_bytes).hex(" ") == mac.hex(" ")
 
     def test_4_1_4(self):
-        packet_hex = """
+        self.check(
+            """
             45 e0 00 87 1f a9 40 00 ff 06 7c 97 ac 1b 1c 1d
             0a 0b 0c 0d 00 b3 e9 d7 11 c1 42 62 fb fb ab 9e
             c0 18 01 00 40 0c 00 00 01 01 08 0a 84 a5 0b f5
@@ -177,73 +127,24 @@ class TestIETFVectors:
             26 02 06 01 04 00 01 00 01 02 02 80 00 02 02 02
             00 02 02 42 00 02 06 41 04 00 00 da c0 02 08 40
             06 00 64 00 01 01 00
-        """
-        traffic_key = bytes.fromhex(
-            """d9 e2 17 e4 83 4a 80 ca 2f 3f d8 de 2e 41 b8 e6 79 7f ea 96"""
+            """,
+            "d9 e2 17 e4 83 4a 80 ca 2f 3f d8 de 2e 41 b8 e6 79 7f ea 96",
+            "a6 3f 0e cb bb 2e 63 5c 95 4d ea c7",
+            self.server_isn_41x,
+            self.client_isn_41x,
         )
-        mac = bytes.fromhex("a6 3f 0e cb bb 2e 63 5c 95 4d ea c7")
-
-        p = IP(bytes.fromhex(packet_hex))
-        context_bytes = build_context_from_scapy(
-            p, self.server_isn_41x, self.client_isn_41x
-        )
-        logger.info("context: %s", context_bytes.hex(" "))
-        assert kdf_sha1(self.master_key, context_bytes).hex(" ") == traffic_key.hex(" ")
-        message_bytes = build_message_from_scapy(p, include_options=True)
-        assert mac_sha1(traffic_key, message_bytes).hex(" ") == mac.hex(" ")
 
     def test_4_2_1(self):
-        packet_hex = """
+        self.check(
+            """
             45 e0 00 4c 53 99 40 00 ff 06 48 e2 0a 0b 0c 0d
             ac 1b 1c 1d ff 12 00 b3 cb 0e fb ee 00 00 00 00
             e0 02 ff ff 54 1f 00 00 02 04 05 b4 01 03 03 08
             04 02 08 0a 00 02 4c ce 00 00 00 00 1d 10 3d 54
             80 af 3c fe b8 53 68 93 7b 8f 9e c2
-        """
-        traffic_key_hex = "30 ea a1 56 0c f0 be 57 da b5 c0 45 22 9f b1 0a 42 3c d7 ea"
-        mac_hex = "80 af 3c fe b8 53 68 93 7b 8f 9e c2"
-        traffic_key = bytes.fromhex(traffic_key_hex)
-        mac = bytes.fromhex(mac_hex)
-
-        p = IP(bytes.fromhex(packet_hex))
-        assert p[IP].src == str(self.client_ipv4)
-        assert p[IP].dst == str(self.server_ipv4)
-        assert p[TCP].flags.S == True
-        assert p[TCP].flags.A == False
-        assert p[TCP].dport == 179
-        assert p[TCP].sport == 0xFF12
-        assert p[TCP].ack == 0
-        assert p[TCP].dataofs == 14
-        assert p[TCP].seq == self.client_isn_42x
-        assert p[TCP].ack == 0
-
-        context_bytes = build_context_from_scapy_syn(p)
-        logger.info("context: %s", context_bytes.hex(" "))
-        assert kdf_sha1(self.master_key, context_bytes).hex() == traffic_key.hex()
-
-        packet_bytes = bytes.fromhex(packet_hex)
-        message_bytes = bytes.fromhex("00 00 00 00")
-        message_bytes += self.client_ipv4.packed
-        message_bytes += self.server_ipv4.packed
-        message_bytes += bytes.fromhex("00 06 00 38")
-        message_bytes += packet_bytes[0x14:0x24]
-        message_bytes += bytes.fromhex("00 00")
-        message_bytes += packet_bytes[0x26:0x28]
-
-        # Even when excluding options the TCP-AO option itself is included with zeroed MAC
-        message_bytes += packet_bytes[0x3C:0x40]
-        message_bytes += b"\x00" * 12
-
-        logger.info("message: %s", message_bytes.hex(" "))
-        assert (
-            message_bytes.hex()
-            == build_message_from_scapy(p, include_options=False).hex()
-        )
-        assert mac_sha1(traffic_key, message_bytes).hex() == mac.hex()
-        self.check(
-            packet_hex,
-            traffic_key_hex,
-            mac_hex,
+            """,
+            "30 ea a1 56 0c f0 be 57 da b5 c0 45 22 9f b1 0a 42 3c d7 ea",
+            "80 af 3c fe b8 53 68 93 7b 8f 9e c2",
             self.client_isn_42x,
             0,
             include_options=False,
