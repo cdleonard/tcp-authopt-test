b5c55a2cc933c43a05d9e496590bb3e6f10661d2
Leonard Crestez <cdleonard@gmail.com>
2021-08-30 22:02:56 +0300

    run.sh: Args go default to pytest, also pass -s --log-cli-level=DEBUG by default
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- run.sh
+++ run.sh
@@ -1,4 +1,8 @@
 #! /bin/bash
+#
+# Create virtualenv using tox and run pytest
+# Accepts all args that pytest does
+#
 
 if ! command -v tox >/dev/null; then
 	echo >&2 "error: please install the python tox package"
@@ -8,4 +12,4 @@ if [[ $(id -u) -ne 0 ]]; then
 	echo >&2 "warning: running as non-root user is unlikely to work"
 fi
 cd "$(dirname "${BASH_SOURCE[0]}")"
-exec tox "$@"
+exec tox -- -s --log-cli-level=DEBUG "$@"
