3f7afdea80f393aeedd9cff125e1db8e7358f82e
Leonard Crestez <cdleonard@gmail.com>
2021-08-30 22:40:01 +0300

    run-venv.sh: New script which does not require tox
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

---
+++ run-venv.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+#
+# Create virtualenv using tox and run pytest
+# Accepts all args that pytest does
+#
+set -e
+
+maybe_sudo=
+if [[ $(id -u) -ne 0 ]]; then
+	echo >&2 "warning: running as non-root user is unlikely to work"
+	maybe_sudo=sudo
+fi
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
+if [[ -d venv ]]; then
+	echo >&2 "Using existing $(readlink -f venv)"
+	. venv/bin/activate
+else
+	echo >&2 "Creating $(readlink -f venv)"
+	python3 -m venv venv
+	. venv/bin/activate
+	pip install -r requirements.txt
+fi
+exec $maybe_sudo pytest -s --log-cli-level=DEBUG "$@"
