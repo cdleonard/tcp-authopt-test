9755ba65a7f80e91e91ae0b44abf72b48b3c256a
Leonard Crestez <cdleonard@gmail.com>
2021-09-03 20:14:11 +0300

    test_sockopt: More tests for get/set TCP_AUTHOPT
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_sockopt.py
+++ tcp_authopt_test/test_sockopt.py
@@ -7,8 +7,11 @@ from ipaddress import IPv4Address, IPv6Address
 
 import pytest
 
+from . import linux_tcp_authopt
 from .linux_tcp_authopt import (
+    TCP_AUTHOPT,
     set_tcp_authopt,
+    get_tcp_authopt,
     set_tcp_authopt_key,
     tcp_authopt,
     tcp_authopt_key,
@@ -72,3 +75,31 @@ def test_tcp_authopt_key_setdel(exit_stack):
     with pytest.raises(OSError) as e:
         set_tcp_authopt_key(sock, key)
     assert e.value.errno == errno.ENOENT
+
+
+def test_get_tcp_authopt(exit_stack):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        with pytest.raises(OSError) as e:
+            sock.getsockopt(socket.SOL_TCP, TCP_AUTHOPT, 4)
+        assert e.value.errno == errno.ENOENT
+
+
+def test_set_get_tcp_authopt_flags(exit_stack):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        # No flags by default
+        set_tcp_authopt(sock, tcp_authopt())
+        opt = get_tcp_authopt(sock)
+        assert opt.flags == 0
+
+        # simple flags are echoed
+        goodflag = linux_tcp_authopt.TCP_AUTHOPT_FLAG_REJECT_UNEXPECTED
+        set_tcp_authopt(sock, tcp_authopt(flags=goodflag))
+        opt = get_tcp_authopt(sock)
+        assert opt.flags == goodflag
+
+        # attempting to set a badflag returns an error and has no effect
+        badflag = 1 << 27
+        with pytest.raises(OSError) as e:
+            set_tcp_authopt(sock, tcp_authopt(flags=badflag))
+        opt = get_tcp_authopt(sock)
+        assert opt.flags == goodflag
