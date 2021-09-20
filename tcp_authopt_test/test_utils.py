8e52c35aacd123ceb5a562fb1b56247d776f06d7
Leonard Crestez <cdleonard@gmail.com>
2021-09-20 15:34:14 +0300

    test_utils: Test check_tcp_authopt_signature break_tcp_authopt_signature
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_utils.py
+++ tcp_authopt_test/test_utils.py
@@ -1,10 +1,11 @@
 import pytest
+from scapy.layers.inet import IP, TCP
+from scapy.layers.l2 import Ether
 from .scapy_utils import tcp_seq_wrap
 
 
 def test_scapy_tcp_seq_rollover():
     """In case anybody wondered scapy does not in fact automate seq rollover"""
-    from scapy.layers.inet import IP, TCP
 
     p = IP() / TCP()
     p[TCP].seq = 0xA0000000 + 0x70000000
@@ -22,3 +23,25 @@ def test_scapy_tcp_seq_rollover():
 def test_tcp_seq_wrap():
     assert hex(tcp_seq_wrap(0xA0000000 + 0x70000000)) == "0x10000000"
     assert hex(tcp_seq_wrap(10 - 12)) == "0xfffffffe"
+
+
+def test_sign_tcp_authopt():
+    from .scapy_tcp_authopt import TcpAuthOptAlg_HMAC_SHA1
+    from .scapy_tcp_authopt import add_tcp_authopt_signature
+    from .scapy_tcp_authopt import check_tcp_authopt_signature
+    from .test_badack_synack import break_tcp_authopt_signature
+
+    alg = TcpAuthOptAlg_HMAC_SHA1()
+    master_key = b"secret"
+    sisn = 1000
+    disn = 0
+
+    p = Ether() / IP() / TCP(flags='S', seq=sisn, ack=disn)
+    add_tcp_authopt_signature(p, alg, master_key, sisn, disn)
+    assert check_tcp_authopt_signature(p, alg, master_key, sisn, disn)
+
+    p1bytes = bytes(p)
+    break_tcp_authopt_signature(p)
+    p2bytes = bytes(p)
+    assert not check_tcp_authopt_signature(p, alg, master_key, sisn, disn)
+    assert Ether(p1bytes)[TCP].chksum != Ether(p2bytes)[TCP].chksum
