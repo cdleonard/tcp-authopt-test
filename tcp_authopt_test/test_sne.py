9eabbd08f7d1a7ff44faede4ced8d6a038c0a0f7
Leonard Crestez <cdleonard@gmail.com>
2021-10-10 00:45:35 +0300

    test_sne: Rewrite to use TCP_REPAIR
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_sne.py
+++ tcp_authopt_test/test_sne.py
@@ -3,50 +3,63 @@
 
 import logging
 from contextlib import ExitStack
+import socket
 
-from .tcp_connection_fixture import TCPConnectionFixture
-from .utils import check_socket_echo
+from .netns_fixture import NamespaceFixture
+from .linux_tcp_repair import get_tcp_repair_recv_send_queue_seq
+from .utils import (
+    DEFAULT_TCP_SERVER_PORT,
+    check_socket_echo,
+    create_client_socket,
+    create_listen_socket,
+)
+from .server import SimpleServerThread
 
 logger = logging.getLogger(__name__)
 
 
 def test_sne(exit_stack: ExitStack):
     """Reproduce a seq/ack overlap"""
-    overflow = 0x1000000
+    overflow = 0x200000
     bufsize = 0x10000
-    con = TCPConnectionFixture(server_thread_kwargs=dict(bufsize=bufsize))
-    exit_stack.enter_context(con)
+    nsfixture = exit_stack.enter_context(NamespaceFixture())
+    server_addr = nsfixture.get_addr(socket.AF_INET, 1)
+    client_addr = nsfixture.get_addr(socket.AF_INET, 2)
+    server_addr_port = (str(server_addr), DEFAULT_TCP_SERVER_PORT)
+    listen_socket = create_listen_socket(
+        ns=nsfixture.server_netns_name,
+        bind_addr=server_addr,
+    )
+    exit_stack.enter_context(listen_socket)
+    server_thread = SimpleServerThread(listen_socket, mode="echo", bufsize=bufsize)
+    exit_stack.enter_context(server_thread)
 
     found = False
 
     client_socket = None
-    for iter in range(1000):
+    for _ in range(10000):
         try:
-            client_socket = con.create_client_socket()
-            client_socket.connect(con.server_addr_port)
-            client_isn, server_isn = con.sniffer_session.get_client_server_isn()
-            logger.info(
-                "iter %d client server ISN %08x %08x",
-                iter,
-                client_isn,
-                server_isn,
+            client_socket = create_client_socket(
+                ns=nsfixture.client_netns_name,
+                bind_addr=client_addr,
             )
+            client_socket.connect(server_addr_port)
 
-            if (
-                client_isn + overflow > 0x100000000
-                or server_isn + overflow > 0x100000000
-            ):
+            recv_seq, send_seq = get_tcp_repair_recv_send_queue_seq(client_socket)
+            if recv_seq + overflow > 0x100000000 or send_seq + overflow > 0x100000000:
                 found = True
                 break
             client_socket.close()
-            con.sniffer_session.wait_close()
-            con.sniffer_session.reset()
             client_socket = None
         finally:
             if not found and client_socket:
                 client_socket.close()
     assert found
 
+    logger.debug("setup recv_seq %08x send_seq %08x", recv_seq, send_seq)
+    logger.info("transfer %d bytes", 2 * overflow)
     for _ in range(2 * overflow // bufsize):
         check_socket_echo(client_socket, bufsize)
-    con.sniffer_session.client_info.sisn
+    new_recv_seq, new_send_seq = get_tcp_repair_recv_send_queue_seq(client_socket)
+    logger.debug("final recv_seq %08x send_seq %08x", new_recv_seq, new_send_seq)
+    assert new_recv_seq < recv_seq or new_send_seq < send_seq
