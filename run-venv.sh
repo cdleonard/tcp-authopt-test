eb691ff4890bbb06539a3d46af439a53f84cce4f
Leonard Crestez <cdleonard@gmail.com>
2021-10-14 09:09:37 +0300

    run-venv.sh: Rewrite to avoid relying on sudo -E
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- run-venv.sh
+++ run-venv.sh
@@ -5,22 +5,28 @@
 #
 set -e
 
-maybe_sudo=
-if [[ $(id -u) -ne 0 ]]; then
-	echo >&2 "warning: running as non-root user, attempting sudo"
-	# sudo -E to use the virtualenv:
-	maybe_sudo="sudo -E"
-fi
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 if [[ -d venv ]]; then
 	echo >&2 "Using existing $(readlink -f venv)"
-	. venv/bin/activate
 else
 	echo >&2 "Creating $(readlink -f venv)"
 	python3 -m venv venv
-	. venv/bin/activate
-	pip install wheel
-	pip install -r requirements.txt
+	(
+		. venv/bin/activate
+		pip install wheel
+		pip install -r requirements.txt
+	)
 fi
-exec $maybe_sudo pytest -s --log-cli-level=DEBUG "$@"
+
+cmd=(pytest -s --log-cli-level=DEBUG "$@")
+if [[ $(id -u) -ne 0 ]]; then
+	echo >&2 "warning: running as non-root user, attempting sudo"
+	# sudo -E to use the virtualenv:
+	cmd=(sudo bash -c ". venv/bin/activate;$(printf " %q" "${cmd[@]}")")
+	set -x
+	exec "${cmd[@]}"
+else
+	. venv/bin/activate
+	exec "${cmd[@]}"
+fi
