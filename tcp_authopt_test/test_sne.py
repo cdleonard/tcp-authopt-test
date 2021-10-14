07517ea17c58a1992eec0acd5e8ac3269dd86d00
Leonard Crestez <cdleonard@gmail.com>
2021-10-14 18:09:32 +0300

    test_sne: Check via tcp_repair_authopt
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_sne.py
+++ tcp_authopt_test/test_sne.py
@@ -1,13 +1,17 @@
 # SPDX-License-Identifier: GPL-2.0
 """Validate SNE implementation for TCP-AO"""
 
-import pytest
 import logging
-from contextlib import ExitStack
 import socket
+from contextlib import ExitStack
 
+import pytest
+
+from .linux_tcp_authopt import set_tcp_authopt_key_kwargs
+from .linux_tcp_repair import get_tcp_repair_recv_send_queue_seq, tcp_repair_toggle
+from .linux_tcp_repair_authopt import get_tcp_repair_authopt
 from .netns_fixture import NamespaceFixture
-from .linux_tcp_repair import get_tcp_repair_recv_send_queue_seq
+from .server import SimpleServerThread
 from .utils import (
     DEFAULT_TCP_SERVER_PORT,
     check_socket_echo,
@@ -15,8 +19,6 @@ from .utils import (
     create_listen_socket,
     socket_set_linger,
 )
-from .linux_tcp_authopt import set_tcp_authopt_key_kwargs
-from .server import SimpleServerThread
 
 logger = logging.getLogger(__name__)
 
@@ -70,6 +72,16 @@ def test_sne(exit_stack: ExitStack, signed: bool):
     assert found
 
     logger.debug("setup recv_seq %08x send_seq %08x", recv_seq, send_seq)
+
+    if signed:
+        with tcp_repair_toggle(client_socket):
+            init_tcp_repair_authopt = get_tcp_repair_authopt(client_socket)
+        assert init_tcp_repair_authopt.src_isn + 1 == send_seq
+        assert init_tcp_repair_authopt.dst_isn + 1 == recv_seq
+        assert init_tcp_repair_authopt.snd_sne == 0
+        assert init_tcp_repair_authopt.rcv_sne == 0
+        logger.debug("tcp repair authopt: %r", init_tcp_repair_authopt)
+
     logger.info("transfer %d bytes", 2 * overflow)
     for _ in range(2 * overflow // bufsize):
         if mode == "recv":
@@ -82,4 +94,15 @@ def test_sne(exit_stack: ExitStack, signed: bool):
 
     new_recv_seq, new_send_seq = get_tcp_repair_recv_send_queue_seq(client_socket)
     logger.debug("final recv_seq %08x send_seq %08x", new_recv_seq, new_send_seq)
+
+    if signed:
+        with tcp_repair_toggle(client_socket):
+            exit_tcp_repair_authopt = get_tcp_repair_authopt(client_socket)
+        logger.debug("exit tcp repair authopt: %r", exit_tcp_repair_authopt)
+        assert exit_tcp_repair_authopt.src_isn == init_tcp_repair_authopt.src_isn
+        assert exit_tcp_repair_authopt.dst_isn == init_tcp_repair_authopt.dst_isn
+        assert (
+            exit_tcp_repair_authopt.snd_sne != 0 or exit_tcp_repair_authopt.rcv_sne != 0
+        )
+
     assert new_recv_seq < recv_seq or new_send_seq < send_seq
