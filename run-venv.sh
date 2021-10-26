2132a73853987ecfe244b87eedea4c2454e80f4c
Leonard Crestez <cdleonard@gmail.com>
2021-10-26 19:03:34 +0300

    run-venv.sh: Remove useless set -x left for debugging
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- run-venv.sh
+++ run-venv.sh
@@ -24,7 +24,6 @@ if [[ $(id -u) -ne 0 ]]; then
 	echo >&2 "warning: running as non-root user, attempting sudo"
 	# sudo -E to use the virtualenv:
 	cmd=(sudo bash -c ". venv/bin/activate;$(printf " %q" "${cmd[@]}")")
-	set -x
 	exec "${cmd[@]}"
 else
 	. venv/bin/activate
