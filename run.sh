71a0d0b442e367b2f8be4c86d3c1021d390bd77f
Leonard Crestez <cdleonard@gmail.com>
2021-10-26 19:03:16 +0300

    run.sh: Automatic sudo
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- run.sh
+++ run.sh
@@ -4,13 +4,18 @@
 # Create virtualenv using tox and run pytest
 # Accepts all args that pytest does
 #
+set -e
+cd "$(dirname "${BASH_SOURCE[0]}")"
 
 if ! command -v tox >/dev/null; then
 	echo >&2 "error: please install the python tox package"
 	exit 1
 fi
+
+cmd=(tox -- -s --log-cli-level=DEBUG --tap-stream "$@")
 if [[ $(id -u) -ne 0 ]]; then
-	echo >&2 "warning: running as non-root user is unlikely to work"
+	echo >&2 "warning: running as non-root user, attempting sudo"
+	exec sudo -- "${cmd[@]}"
+else
+	exec "${cmd[@]}"
 fi
-cd "$(dirname "${BASH_SOURCE[0]}")"
-exec tox -- -s --log-cli-level=DEBUG --tap-stream "$@"
