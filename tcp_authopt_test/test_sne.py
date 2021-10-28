5d9b85915aafb7534b9111dfd2a36dc5d75e30d9
Leonard Crestez <cdleonard@gmail.com>
2021-10-28 14:34:49 +0300

    test_high_seq_rollover: Add validation support
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_sne.py
+++ tcp_authopt_test/test_sne.py
@@ -4,6 +4,7 @@
 import logging
 import socket
 from contextlib import ExitStack
+from ipaddress import ip_address
 
 import pytest
 
@@ -11,6 +12,8 @@ from .linux_tcp_authopt import set_tcp_authopt_key_kwargs
 from .linux_tcp_repair import get_tcp_repair_recv_send_queue_seq, tcp_repair_toggle
 from .linux_tcp_repair_authopt import get_tcp_repair_authopt
 from .netns_fixture import NamespaceFixture
+from .scapy_conntrack import TCPConnectionKey, TCPConnectionTracker
+from .scapy_utils import AsyncSnifferContext, create_capture_socket, tcp_seq_wrap
 from .server import SimpleServerThread
 from .utils import (
     DEFAULT_TCP_SERVER_PORT,
@@ -19,8 +22,40 @@ from .utils import (
     create_listen_socket,
     socket_set_linger,
 )
+from .validator import TcpAuthValidator, TcpAuthValidatorKey
 
 logger = logging.getLogger(__name__)
+
+
+def add_connection_info(
+    tracker: TCPConnectionTracker,
+    saddr,
+    daddr,
+    sport,
+    dport,
+    sisn,
+    disn,
+):
+    client2server_key = TCPConnectionKey(
+        saddr=saddr,
+        daddr=daddr,
+        sport=sport,
+        dport=dport,
+    )
+    client2server_conn = tracker.get_or_create(client2server_key)
+    client2server_conn.sisn = sisn
+    client2server_conn.disn = disn
+    client2server_conn.snd_sne.reset(sisn)
+    client2server_conn.rcv_sne.reset(disn)
+    client2server_conn.found_syn = True
+    client2server_conn.found_synack = True
+    server2client_conn = tracker.get_or_create(client2server_key.rev())
+    server2client_conn.sisn = disn
+    server2client_conn.disn = sisn
+    server2client_conn.snd_sne.reset(disn)
+    server2client_conn.rcv_sne.reset(sisn)
+    server2client_conn.found_syn = True
+    server2client_conn.found_synack = True
 
 
 @pytest.mark.parametrize("signed", [False, True])
@@ -37,6 +72,7 @@ def test_high_seq_rollover(exit_stack: ExitStack, signed: bool):
     bufsize = 0x10000
     secret_key = b"12345"
     mode = "echo"
+    validator_enabled = True
     tcp_repair_authopt_enabled = True
 
     nsfixture = exit_stack.enter_context(NamespaceFixture())
@@ -109,6 +145,33 @@ def test_high_seq_rollover(exit_stack: ExitStack, signed: bool):
         assert init_tcp_repair_authopt.rcv_sne == 0
         logger.debug("tcp repair authopt: %r", init_tcp_repair_authopt)
 
+    # Init validator
+    if signed and validator_enabled:
+        capture_filter = f"tcp port {DEFAULT_TCP_SERVER_PORT}"
+        capture_socket = create_capture_socket(
+            ns=nsfixture.client_netns_name,
+            iface="veth0",
+            filter=capture_filter,
+        )
+        sniffer = exit_stack.enter_context(
+            AsyncSnifferContext(opened_socket=capture_socket)
+        )
+        validator = TcpAuthValidator()
+        validator.keys.append(
+            TcpAuthValidatorKey(key=secret_key, alg_name="HMAC-SHA-1-96")
+        )
+
+        # SYN+SYNACK is not captured so initialize connection info manually
+        add_connection_info(
+            validator.tracker,
+            saddr=ip_address(client_addr),
+            daddr=ip_address(server_addr),
+            dport=client_socket.getpeername()[1],
+            sport=client_socket.getsockname()[1],
+            sisn=tcp_seq_wrap(send_seq - 1),
+            disn=tcp_seq_wrap(recv_seq - 1),
+        )
+
     logger.info("transfer %d bytes", 2 * overflow)
     fail_transfer = False
     for iternum in range(2 * overflow // bufsize):
@@ -128,6 +191,25 @@ def test_high_seq_rollover(exit_stack: ExitStack, signed: bool):
     new_recv_seq, new_send_seq = get_tcp_repair_recv_send_queue_seq(client_socket)
     logger.debug("final recv_seq %08x send_seq %08x", new_recv_seq, new_send_seq)
     assert new_recv_seq < recv_seq or new_send_seq < send_seq
+
+    # Validate capture
+    if signed and validator_enabled:
+        sniffer.stop()
+        for p in sniffer.results:
+            validator.handle_packet(p)
+        # Allow incomplete connections from FIN/ACK of connections dropped
+        # because of low seq/ack
+        validator.raise_errors(allow_incomplete=True)
+        client_scappy_key = TCPConnectionKey(
+            saddr=ip_address(client_addr),
+            daddr=ip_address(server_addr),
+            dport=client_socket.getpeername()[1],
+            sport=client_socket.getsockname()[1],
+        )
+        client_scappy_conn = validator.tracker.get(client_scappy_key)
+        snd_sne_rollover = client_scappy_conn.snd_sne.sne != 0
+        rcv_sne_rollover = client_scappy_conn.rcv_sne.sne != 0
+        assert snd_sne_rollover or rcv_sne_rollover
 
     # Validate SNE as read via TCP_REPAIR_AUTHOPT
     if signed and tcp_repair_authopt_enabled:
