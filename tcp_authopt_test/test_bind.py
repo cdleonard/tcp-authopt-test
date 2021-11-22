b30cd846833e4977512e06cac4a5c97883eca085
Leonard Crestez <cdleonard@gmail.com>
2021-11-22 20:58:29 +0200

    test_bind: Use more recent helpers to reduce verbosity
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_bind.py
+++ tcp_authopt_test/test_bind.py
@@ -9,12 +9,9 @@ from .conftest import raises_optional_exception, skipif_missing_tcp_authopt
 from .linux_tcp_authopt import (
     TCP_AUTHOPT_ALG,
     TCP_AUTHOPT_FLAG,
-    TCP_AUTHOPT_KEY_FLAG,
     set_tcp_authopt,
-    set_tcp_authopt_key,
     set_tcp_authopt_key_kwargs,
     tcp_authopt,
-    tcp_authopt_key,
 )
 from .netns_fixture import NamespaceFixture
 from .server import SimpleServerThread
@@ -23,7 +20,6 @@ from .utils import (
     check_socket_echo,
     create_client_socket,
     create_listen_socket,
-    netns_context,
 )
 
 pytestmark = skipif_missing_tcp_authopt
@@ -44,40 +40,37 @@ def test_addr_server_bind(exit_stack: ExitStack, address_family):
     exit_stack.enter_context(SimpleServerThread(listen_socket, mode="echo"))
 
     # set keys:
-    server_key = tcp_authopt_key(
-        alg=TCP_AUTHOPT_ALG.HMAC_SHA_1_96,
+    set_tcp_authopt_key_kwargs(
+        listen_socket,
         key="hello",
-        flags=TCP_AUTHOPT_KEY_FLAG.BIND_ADDR,
         addr=client_addr2,
     )
     set_tcp_authopt(
         listen_socket,
         tcp_authopt(flags=TCP_AUTHOPT_FLAG.REJECT_UNEXPECTED),
     )
-    set_tcp_authopt_key(listen_socket, server_key)
 
     # create client socket:
-    def _create_client_socket():
-        with netns_context(nsfixture.client_netns_name):
-            client_socket = socket.socket(address_family, socket.SOCK_STREAM)
-        client_key = tcp_authopt_key(
+    def _create_client_socket(bind_addr):
+        client_socket = create_client_socket(
+            ns=nsfixture.client_netns_name,
+            family=address_family,
+            bind_addr=bind_addr,
+        )
+        set_tcp_authopt_key_kwargs(
+            client_socket,
             alg=TCP_AUTHOPT_ALG.HMAC_SHA_1_96,
             key="hello",
         )
-        set_tcp_authopt_key(client_socket, client_key)
         return client_socket
 
     # addr match:
-    with _create_client_socket() as client_socket2:
-        client_socket2.bind((client_addr2, 0))
-        client_socket2.settimeout(1.0)
+    with _create_client_socket(client_addr2) as client_socket2:
         client_socket2.connect((server_addr, DEFAULT_TCP_SERVER_PORT))
 
     # addr mismatch:
-    with _create_client_socket() as client_socket1:
-        client_socket1.bind((client_addr, 0))
+    with _create_client_socket(client_addr) as client_socket1:
         with pytest.raises(socket.timeout):
-            client_socket1.settimeout(1.0)
             client_socket1.connect((server_addr, DEFAULT_TCP_SERVER_PORT))
 
 
@@ -108,45 +101,26 @@ def test_addr_client_bind(exit_stack: ExitStack, address_family):
     exit_stack.enter_context(SimpleServerThread(listen_socket2, mode="echo"))
 
     # set keys:
-    set_tcp_authopt_key(
-        listen_socket1,
-        tcp_authopt_key(
-            alg=TCP_AUTHOPT_ALG.HMAC_SHA_1_96,
-            key="11111",
-        ),
-    )
-    set_tcp_authopt_key(
-        listen_socket2,
-        tcp_authopt_key(
-            alg=TCP_AUTHOPT_ALG.HMAC_SHA_1_96,
-            key="22222",
-        ),
-    )
+    set_tcp_authopt_key_kwargs(listen_socket1, key="11111")
+    set_tcp_authopt_key_kwargs(listen_socket2, key="22222")
 
     # create client socket:
     def _create_client_socket():
-        with netns_context(nsfixture.client_netns_name):
-            client_socket = socket.socket(address_family, socket.SOCK_STREAM)
-        set_tcp_authopt_key(
-            client_socket,
-            tcp_authopt_key(
-                alg=TCP_AUTHOPT_ALG.HMAC_SHA_1_96,
-                key="11111",
-                flags=TCP_AUTHOPT_KEY_FLAG.BIND_ADDR,
-                addr=server_addr1,
-            ),
+        client_socket = create_client_socket(
+            ns=nsfixture.client_netns_name,
+            family=address_family,
+            bind_addr=client_addr,
         )
-        set_tcp_authopt_key(
+        set_tcp_authopt_key_kwargs(
             client_socket,
-            tcp_authopt_key(
-                alg=TCP_AUTHOPT_ALG.HMAC_SHA_1_96,
-                key="22222",
-                flags=TCP_AUTHOPT_KEY_FLAG.BIND_ADDR,
-                addr=server_addr2,
-            ),
+            key="11111",
+            addr=server_addr1,
         )
-        client_socket.settimeout(1.0)
-        client_socket.bind((client_addr, 0))
+        set_tcp_authopt_key_kwargs(
+            client_socket,
+            key="22222",
+            addr=server_addr2,
+        )
         return client_socket
 
     with _create_client_socket() as client_socket1:
