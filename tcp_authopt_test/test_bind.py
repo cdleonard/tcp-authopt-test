fc27590681389965e843dbeaf69f71b6d77b8bc0
Leonard Crestez <cdleonard@gmail.com>
2021-11-22 22:00:02 +0200

    test_bind: Minimal prefixlen test
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_bind.py
+++ tcp_authopt_test/test_bind.py
@@ -131,6 +131,51 @@ def test_addr_client_bind(exit_stack: ExitStack, address_family):
         check_socket_echo(client_socket2)
 
 
+@pytest.mark.parametrize("address_family", [socket.AF_INET, socket.AF_INET6])
+def test_server_prefixlen(exit_stack: ExitStack, address_family):
+    """Server key prefixlen is *.2/31, client can connect from .3 but not from .1"""
+    nsfixture = exit_stack.enter_context(NamespaceFixture())
+    server_addr = str(nsfixture.get_server_addr(address_family))
+    client_addr1 = str(nsfixture.get_client_addr(address_family, 1))
+    client_addr3 = str(nsfixture.get_client_addr(address_family, 3))
+
+    listen_socket = create_listen_socket(
+        family=address_family,
+        ns=nsfixture.server_netns_name,
+        bind_addr=server_addr,
+    )
+    listen_socket = exit_stack.enter_context(listen_socket)
+    exit_stack.enter_context(SimpleServerThread(listen_socket, mode="echo"))
+
+    # server key:
+    prefixlen = 31 if address_family == socket.AF_INET else 127
+    server_key_bind_addr = nsfixture.get_client_addr(address_family, 2)
+    set_tcp_authopt_key_kwargs(
+        listen_socket,
+        key="11111",
+        prefixlen=prefixlen,
+        addr=server_key_bind_addr,
+    )
+    server_addr_port = (server_addr, DEFAULT_TCP_SERVER_PORT)
+
+    # create client socket:
+    def _create_client_socket(**kw):
+        client_socket = create_client_socket(
+            ns=nsfixture.client_netns_name,
+            family=address_family,
+            **kw,
+        )
+        set_tcp_authopt_key_kwargs(client_socket, key="11111")
+        return client_socket
+
+    with _create_client_socket(bind_addr=client_addr3) as client_socket:
+        client_socket.connect(server_addr_port)
+        check_socket_echo(client_socket)
+    with _create_client_socket(bind_addr=client_addr1) as client_socket:
+        with pytest.raises(socket.timeout):
+            client_socket.connect(server_addr_port)
+
+
 @pytest.mark.parametrize("samekeyid", [True, False])
 def test_sign_bad_keyid(exit_stack: ExitStack, samekeyid: bool):
     """Client and server have different keyid, should fail"""
