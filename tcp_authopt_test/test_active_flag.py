89de188ce6fa2161eee04efd497e7099902b5563
Leonard Crestez <cdleonard@gmail.com>
2022-03-28 20:50:51 +0300

    hack
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_active_flag.py
+++ tcp_authopt_test/test_active_flag.py
@@ -1,5 +1,8 @@
 import logging
+import waiting
 from contextlib import ExitStack
+
+from tcp_authopt_test.server import SimpleServerThread
 
 from .linux_tcp_authopt import (
     TCP_AUTHOPT_FLAG,
@@ -11,6 +14,12 @@ from .tcp_connection_fixture import TCPConnectionFixture
 
 logger = logging.getLogger(__name__)
 
+
+def wait_server_sock(server_thread: SimpleServerThread):
+    def has_server_socket():
+        return len(server_thread.server_socket) > 0
+
+    waiting.wait(has_server_socket, sleep_seconds=0.1, timeout_seconds=5)
 
 def test_active_on(exit_stack: ExitStack):
     con = TCPConnectionFixture(enable_sniffer=False)
@@ -27,8 +36,13 @@ def test_active_on(exit_stack: ExitStack):
     )
 
     client_socket.connect(con.server_addr_port)
+    wait_server_sock(con.server_thread)
+    server_socket = con.server_thread.server_socket[0]
+
     client_info = get_tcp_authopt(client_socket)
     assert client_info.flags & TCP_AUTHOPT_FLAG.ACTIVE != 0
+    server_info = get_tcp_authopt(server_socket)
+    assert server_info.flags & TCP_AUTHOPT_FLAG.ACTIVE == 0
 
 
 def test_active_off(exit_stack: ExitStack):
@@ -42,6 +56,11 @@ def test_active_off(exit_stack: ExitStack):
     )
 
     client_socket.connect(con.server_addr_port)
+    wait_server_sock(con.server_thread)
+    server_socket = con.server_thread.server_socket[0]
     client_info = get_tcp_authopt(client_socket)
     assert client_info is not None
     assert client_info.flags & TCP_AUTHOPT_FLAG.ACTIVE == 0
+    server_info = get_tcp_authopt(server_socket)
+    assert server_info is not None
+    assert server_info.flags & TCP_AUTHOPT_FLAG.ACTIVE == 0
