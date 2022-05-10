0013cf50be72cf6647faf7bfbef354f9cfcef2e4
Leonard Crestez <cdleonard@gmail.com>
2022-05-10 16:14:17 +0000

    test_sock_destroy: Test SOCK_DESTROY for TCP_TIME_WAIT sockets
    Signed-off-by: Leonard Crestez <cdleonard@gmail.com>

--- tcp_authopt_test/test_sock_destroy.py
+++ tcp_authopt_test/test_sock_destroy.py
@@ -29,3 +29,24 @@ def test_estab(exit_stack: ExitStack, address_family):
         check_socket_echo(con.client_socket)
     assert e.value.errno == errno.ECONNRESET
     con.server_thread.raise_exception_on_exit = False
+
+
+@pytest.mark.parametrize("address_family", [socket.AF_INET, socket.AF_INET6])
+def test_tw(exit_stack: ExitStack, address_family):
+    con = TCPConnectionFixture(address_family=address_family)
+    exit_stack.enter_context(con)
+
+    con.client_socket.connect(con.server_addr_port)
+    check_socket_echo(con.client_socket)
+    assert con.get_client_tcp_state() == "ESTAB"
+    assert con.get_server_tcp_state() == "ESTAB"
+
+    con.client_socket.close()
+    assert con.get_client_tcp_state() == "TIME-WAIT"
+    assert con.get_server_tcp_state() is None
+
+    # Kill the client side TIME-WAIT socket
+    ss = "/home/vagrant/iproute2/misc/ss"
+    script = f"ss --net {con.client_netns_name} --tcp --kill state all"
+    subprocess.run(script, shell=True, check=True)
+    assert con.get_client_tcp_state() is None
